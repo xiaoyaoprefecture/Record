8164ac65209b2a74f8c2ae56c979ae5b630fd6e0
xiaoyaoprefecture <861712150@qq.com>
2017-04-06 00:40:23 +0800

    模拟实现文件模式的录音

---
+++ app/src/main/java/com/xiaoyaoprefecture/imapplication/StreamActivity.java
@@ -0,0 +1,13 @@
+package com.xiaoyaoprefecture.imapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StreamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stream);
+    }
+}
